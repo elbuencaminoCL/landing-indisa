f6ec18b00ce7e5a2d4dfcb4f8ef6e996698156c1
Jair <ejairn@gmail.com>
2017-01-03 14:18:21 -0500

    Banner

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -2,26 +2,34 @@
 	let $headerMenu = $( '#headerMenu' );
 	let $headerList = $( '#headerList' );
 	let $headerContainer = $( '#headerContainer' );
-	let $headerLogo = $( '.header__logo' );
+	let $headerLogoFigure = $( '.header__logo' );
+	let $headerLogo = $( '#headerLogo' );
 
 	$headerMenu.on( 'click', () => {
 		$headerMenu.toggleClass( 'visible' );
 		$headerList.toggleClass( 'visible' );
+		$headerLogo.toggleClass( 'visible' );
 		if( $headerMenu.hasClass( 'visible' ) ) {
-			$headerLogo.removeClass( 'text-center' );
+			$headerLogoFigure.removeClass( 'text-center' );
 		}
 		else {
-			$headerLogo.addClass( 'text-center' );
+			$headerLogoFigure.addClass( 'text-center' );
 		}
 	});
 
 
 	$headerContainer.on('sticky.zf.stuckto:top', function() {
+
 		$headerMenu.addClass( 'visible' );
 		$headerList.addClass( 'visible' );
-		$headerLogo.removeClass( 'text-center' );
+		$headerLogoFigure.removeClass( 'text-center' );
+		$headerLogo.addClass( 'visible' );
+
 	}).on('sticky.zf.unstuckfrom:top', function(){
-		$headerLogo.addClass( 'text-center' );
+
+		$headerLogoFigure.addClass( 'text-center' );
 	  $headerMenu.removeClass( 'visible' );
 	  $headerList.removeClass( 'visible' );
+		$headerLogo.removeClass( 'visible' );
+
 	})
